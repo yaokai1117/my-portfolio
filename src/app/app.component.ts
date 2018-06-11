3c3570621e33dcc8eb205c61c86ef77b3e96c088
Kai Yao <yaokai1117@gmail.com>
2018-06-10 23:15:54 -0700

    add experiences and eduaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Experience} from './experience/experience.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  experiences: Array<Experience> = [
+    {duration: '2018.5 - 2018.8', title: 'Software Engineering Intern', company: 'Google', description: 'Worked on the frontend of AdWords Next. Implement' +
+      'ed review panels for the ads construction page.'},
+    {duration: '2016.6 - 2017.5', title: 'Research Intern', company: 'Microsoft Research Asia ', description: 'Worked on the improvement ' +
+      'of the LUIS service (part of the Microsoft Cognitive Services, provides APIs for chatting bots). Built the datetime extractor, out' +
+      'performed the original one in most test cases.'}
+  ];
+
+  education: Array<Experience> = [
+    {duration: '2017.8 - Present', title: 'Master\'s Degree in Software Engineering', company: 'Carnegie Mellon University',
+      description: 'Selected Coursework: Foundation of Software Engineering, Software Architecture & Design, Distributed System, Advance Machine Learning'},
+    {duration: '2013.9 - 2017.7', title: 'Bachelor\'s Degree in Computer Science', company: 'University of Science & Technology of China',
+      description: 'Selected Coursework: Operating System, Computer Networks, Foundation of Algorithm, Principles and Techniques of Compiler'}
+  ];
 }
--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {s} from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-experience',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent implements OnInit {
+  private _experiences: Array<Experience>;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  @Input()
+  set experiences(experiences: Array<Experience>) {
+    this._experiences = experiences;
+  }
+
+  get experiences(): Array<Experience> {
+    return this._experiences;
+  }
+}
+
+export class Experience {
+  duration: string;
+  title: string;
+  company: string;
+  description: string;
+
+  constructor(duration: string, title: string, company: string, description: string) {
+    this.duration = duration;
+    this.title = title;
+    this.company = company;
+    this.description = description;
+  }
 }
